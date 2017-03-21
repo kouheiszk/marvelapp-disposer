b2845d2edce552dbb8eceef1da1b7787a254b666
Kouhei Suzuki <kouhei.szk@gmail.com>
2017-03-21 14:21:25 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+$(function(){
+  setInterval(function() {
+    $("#marvel-prototype-resize-root > aside").hide();
+    $(".position-fixed.userSelect-none.zi-300.fontSize-s").hide();
+    $(".position-fixed.overflow-hidden.verticallyCenter-absolute").hide();
+  }, 100);
+});
